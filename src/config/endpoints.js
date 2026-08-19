dcaaf8a1995ca5075941eb561367a52ee1cb7eea
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2026-08-19 20:49:49 +0530

    Update AWS deployment IP to 13.234.239.90

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,4 +1,4 @@
-export const USER_SERVICE = "https://65.2.10.164.sslip.io";
-export const CART_SERVICE = "https://65.2.10.164.sslip.io";
-export const ORDER_SERVICE = "https://65.2.10.164.sslip.io";
-export const QUERY_SERVICE = "https://65.2.10.164.sslip.io";
+export const USER_SERVICE = "https://13.234.239.90.sslip.io";
+export const CART_SERVICE = "https://13.234.239.90.sslip.io";
+export const ORDER_SERVICE = "https://13.234.239.90.sslip.io";
+export const QUERY_SERVICE = "https://13.234.239.90.sslip.io";
